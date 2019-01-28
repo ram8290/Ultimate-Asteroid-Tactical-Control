ffb355bba5e50c2fbc62a0342dc5f8e4021ef619
Ricardo Martinez <ram8290@uat.edu>
2019-01-27 22:23:14 -0700

    first commit
    sprite, soon controls.

---
+++ Assets/Assets/scripts/mover.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mover : MonoBehaviour {
+
+    public float speed;
+    private Transform ship;
+	// Use this for initialization
+	void Start () {
+        ship = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			Debug.Log ("left arrow");
+            ship.position = ship.position + (Vector3.left * speed);
+		}
+		if (Input.GetKey (KeyCode.RightArrow))
+		{
+			Debug.Log ("right arrow");
+            ship.position = ship.position + (Vector3.right * speed);
+        }
+		if (Input.GetKey (KeyCode.UpArrow))
+		{
+			Debug.Log ("up arrow");
+            ship.position = ship.position + (Vector3.up * speed);
+        }
+		if (Input.GetKey (KeyCode.DownArrow))
+		{
+			Debug.Log ("down arrow");
+            ship.position = ship.position + (Vector3.down * speed);
+        }
+		if (Input.GetKey (KeyCode.Space))
+		{
+            Debug.Log("spacebar");
+            ship.position = new Vector3(0, 0, 0);
+		}
+	}
+}
---
+++ Assets/Assets/scripts/movmentDisable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movmentDisable : MonoBehaviour
+{
+
+    private mover move;
+    // Use this for initialization
+    void Start()
+    {
+        move = GetComponent<mover>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            if (move.enabled == true)
+            {
+                Debug.Log("Pause");
+                move.enabled = false;
+            }
+            else if (move.enabled == false)
+            {
+                Debug.Log("Unpause");
+                move.enabled = true;
+            }
+        }
+       
+    }
+}
